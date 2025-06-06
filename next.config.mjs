6e1e2ee973eb34e2d4ee3ec12a2a4a9d07d8c1e2
123-emu-123 <110202046@g.nccu.edu.tw>
2025-06-06 18:54:41 +0800

    t

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, // 告訴 Next 使用 `app/` 資料夾作為 App Router
+    },
+  };
+  
+  module.exports = nextConfig;
+  
